39bcc54add238cea7c792976cd983ec2f8b6ca6e
numme <nummer14@naver.com>
2025-06-10 11:19:23 +0900

    Second Commit

---
+++ src/MasterfulMain.java
@@ -0,0 +1,11 @@
+class Person {
+    public static void main(String[] args) {
+        System.out.println("hello person");
+    }
+}
+
+class Animal {
+    public static void main(String[] args) {
+        System.out.println("hello animal");
+    }
+}
